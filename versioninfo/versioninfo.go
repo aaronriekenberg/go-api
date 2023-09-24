38843f29a6766274e7c0c13ec1e28f1f8fcf4e54
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-09-24 07:09:44 -0500

    Simplify, include GOARCH and GOOS.

--- versioninfo/versioninfo.go
+++ versioninfo/versioninfo.go
@@ -11,10 +11,23 @@ import (
 	"github.com/aaronriekenberg/go-api/utils"
 )
 
-var buildInfoMap map[string]string
+func buildInfoMap() map[string]string {
+	buildInfoMap := make(map[string]string)
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		buildInfoMap["GoVersion"] = buildInfo.GoVersion
+		for _, setting := range buildInfo.Settings {
+			if strings.HasPrefix(setting.Key, "vcs") || (setting.Key == "GOARCH") || (setting.Key == "GOOS") {
+				buildInfoMap[setting.Key] = setting.Value
+			}
+		}
+	}
+
+	return buildInfoMap
+}
 
 func versionInfoHandlerFunc() http.HandlerFunc {
-	jsonBytes, err := json.Marshal(buildInfoMap)
+	jsonBytes, err := json.Marshal(buildInfoMap())
 	if err != nil {
 		slog.Error("versionInfoHandlerFunc json.Marshal error",
 			"error", err)
@@ -26,17 +39,4 @@ func versionInfoHandlerFunc() http.HandlerFunc {
 
 func NewVersionInfoHandler() http.Handler {
 	return versionInfoHandlerFunc()
-}
-
-func init() {
-	buildInfoMap = make(map[string]string)
-
-	if buildInfo, ok := debug.ReadBuildInfo(); ok {
-		buildInfoMap["GoVersion"] = buildInfo.GoVersion
-		for _, setting := range buildInfo.Settings {
-			if strings.HasPrefix(setting.Key, "vcs") {
-				buildInfoMap[setting.Key] = setting.Value
-			}
-		}
-	}
 }
