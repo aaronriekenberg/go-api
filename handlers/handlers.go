c5cc3a9d02e0fdf1f5c860c199b54cff7d3a9381
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-09-30 07:35:07 -0500

    Simplify.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,37 +3,41 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+
 	"github.com/aaronriekenberg/go-api/config"
 	"github.com/aaronriekenberg/go-api/handlers/command"
 	"github.com/aaronriekenberg/go-api/handlers/requestinfo"
 	"github.com/aaronriekenberg/go-api/handlers/versioninfo"
-	"github.com/julienschmidt/httprouter"
 )
 
 func CreateHandlers(
 	config config.Configuration,
-) (http.Handler, error) {
+) (handler http.Handler, err error) {
+	const get = http.MethodGet
+
 	router := httprouter.New()
 
 	allCommandsHandler, err := command.NewAllCommandsHandler(config.CommandConfiguration)
 	if err != nil {
-		return nil, err
+		return
 	}
-	router.Handler(http.MethodGet, "/api/v1/commands", allCommandsHandler)
+	router.Handler(get, "/api/v1/commands", allCommandsHandler)
 
 	runCommandsHandler, err := command.NewRunCommandsHandler(config.CommandConfiguration)
 	if err != nil {
-		return nil, err
+		return
 	}
-	router.Handler(http.MethodGet, "/api/v1/commands/:id", runCommandsHandler)
+	router.Handler(get, "/api/v1/commands/:id", runCommandsHandler)
 
-	router.Handler(http.MethodGet, "/api/v1/request_info", requestinfo.NewRequestInfoHandler())
+	router.Handler(get, "/api/v1/request_info", requestinfo.NewRequestInfoHandler())
 
 	versionInfoHandler, err := versioninfo.NewVersionInfoHandler()
 	if err != nil {
-		return nil, err
+		return
 	}
-	router.Handler(http.MethodGet, "/api/v1/version_info", versionInfoHandler)
+	router.Handler(get, "/api/v1/version_info", versionInfoHandler)
 
-	return router, nil
+	handler = router
+	return
 }
