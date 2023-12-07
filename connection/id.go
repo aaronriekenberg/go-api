27a5c817ae276cb1a339180fbbe50cd199cc593e
Aaron.Riekenberg <aaron.riekenberg@target.com>
2023-12-07 05:52:06 +0530

    Break into more files.

---
+++ connection/id.go
@@ -0,0 +1,26 @@
+package connection
+
+import "context"
+
+type ConnectionID uint64
+
+type connectionIDContextKey struct {
+}
+
+func AddConnectionIDToContext(
+	ctx context.Context,
+	connectionID ConnectionID,
+) context.Context {
+	key := connectionIDContextKey{}
+	value := &connectionID
+
+	return context.WithValue(ctx, key, value)
+}
+
+func GetConnectionIDFromContext(
+	ctx context.Context,
+) *ConnectionID {
+	key := connectionIDContextKey{}
+
+	return ctx.Value(key).(*ConnectionID)
+}
