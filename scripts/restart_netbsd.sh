cbdfad0a9388f5d882c902ed66f911c243b5bea9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-03-22 15:03:44 -0500

    Avoid ~/bin needing to be in PATH.

--- scripts/restart_netbsd.sh
+++ scripts/restart_netbsd.sh
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/bash
 
-cd /home/aaron/go-api
+cd ~/go-api
 
 pkill go-api
 
-nohup ./go-api ./configfiles/rpi.toml 2>&1 | simplerotate logs &
+nohup ./go-api ./configfiles/rpi.toml 2>&1 | ~/bin/simplerotate logs &
