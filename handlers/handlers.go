f8296a84cf6dcc1aa6990324ee50f7e54b56e52c
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-07-27 05:06:46 -0500

    Add noopReader, more logging in maxRequestBodyLengthHandler.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -52,20 +52,37 @@ func maxRequestBodyLengthHandler(
 	nextHandler http.Handler,
 ) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r2 := *r
 
-		r2.Body = http.MaxBytesReader(w, r.Body, 0)
+		bodyReader := http.MaxBytesReader(w, r.Body, 0)
 
-		_, err := io.ReadAll(r2.Body)
+		_, err := io.ReadAll(bodyReader)
 		if err != nil {
 			slog.Warn("request body read error",
 				"error", err,
-				"content-length", r2.Header.Get("Content-Length"),
+				"url", r.URL.String(),
+				"method", r.Method,
+				"proto", r.Proto,
+				"header", r.Header,
+				"remote_addr", r.RemoteAddr,
 			)
 			utils.HTTPErrorStatusCode(w, http.StatusBadRequest)
 			return
 		}
 
+		r2 := *r
+
+		r2.Body = noopReader{}
+
 		nextHandler.ServeHTTP(w, &r2)
 	})
+}
+
+type noopReader struct{}
+
+func (noopReader noopReader) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+func (noopReader noopReader) Close() error {
+	return nil
 }
