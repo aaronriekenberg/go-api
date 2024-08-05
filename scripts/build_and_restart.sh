ecbbe838ed78cc7e6d483c1c863a4c84d18bee7c
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-05 17:14:38 -0500

    Separate lines

--- scripts/build_and_restart.sh
+++ scripts/build_and_restart.sh
@@ -9,7 +9,8 @@ systemctl --user stop go-api.service
 
 git pull -v
 
-time go test -test.v ./... && time go build -x
+time go test -test.v ./...
+time go build -x
 
 sudo setcap cap_net_bind_service=+ep ./go-api
 
