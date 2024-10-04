37710803c1cdce4a3ead98af5415b50c8db45ac2
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-10-04 04:29:16 -0500

    Move listenerWrapper to own file.

---
+++ server/listenerwrapper.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+
+	"github.com/aaronriekenberg/go-api/config"
+	"github.com/aaronriekenberg/go-api/connection"
+)
+
+type listenerWrapper struct {
+	net.Listener
+}
+
+func (lw *listenerWrapper) Accept() (net.Conn, error) {
+	conn, err := lw.Listener.Accept()
+
+	if err != nil {
+		return conn, err
+	}
+
+	switch conn := conn.(type) {
+	case *net.TCPConn:
+		connectionInfo := connection.ConnectionManagerInstance().AddConnection("tcp")
+
+		slog.Debug("listenerWrapper.Accept got new tcp connection",
+			"connectionID", connectionInfo.ID(),
+		)
+
+		return &tcpConnWrapper{
+			TCPConn:        conn,
+			connectionInfo: connectionInfo,
+		}, nil
+	case *net.UnixConn:
+		connectionInfo := connection.ConnectionManagerInstance().AddConnection("unix")
+
+		slog.Debug("listenerWrapper.Accept got new unix connection",
+			"connectionID", connectionInfo.ID(),
+		)
+
+		return &unixConnWrapper{
+			UnixConn:       conn,
+			connectionInfo: connectionInfo,
+		}, nil
+	default:
+		slog.Warn("listenerWrapper.Accept got unknown conn type",
+			"conn", conn,
+		)
+		return conn, nil
+	}
+}
+
+func createListener(
+	config config.ServerListenerConfiguration,
+) (net.Listener, error) {
+	if config.Network == "unix" {
+		os.Remove(config.ListenAddress)
+	}
+
+	listener, err := net.Listen(config.Network, config.ListenAddress)
+	if err != nil {
+		return nil, fmt.Errorf("net.Listen error: %w", err)
+	}
+
+	listenerWrapper := &listenerWrapper{
+		Listener: listener,
+	}
+
+	return listenerWrapper, nil
+}
--- server/server.go
+++ server/server.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
-	"os"
 	"time"
 
 	"golang.org/x/net/http2"
@@ -16,67 +15,6 @@ import (
 	"github.com/aaronriekenberg/go-api/connection"
 	"github.com/aaronriekenberg/go-api/request"
 )
-
-type listenerWrapper struct {
-	net.Listener
-}
-
-func (lw *listenerWrapper) Accept() (net.Conn, error) {
-	conn, err := lw.Listener.Accept()
-
-	if err != nil {
-		return conn, err
-	}
-
-	switch conn := conn.(type) {
-	case *net.TCPConn:
-		connectionInfo := connection.ConnectionManagerInstance().AddConnection("tcp")
-
-		slog.Debug("listenerWrapper.Accept got new tcp connection",
-			"connectionID", connectionInfo.ID(),
-		)
-
-		return &tcpConnWrapper{
-			TCPConn:        conn,
-			connectionInfo: connectionInfo,
-		}, nil
-	case *net.UnixConn:
-		connectionInfo := connection.ConnectionManagerInstance().AddConnection("unix")
-
-		slog.Debug("listenerWrapper.Accept got new unix connection",
-			"connectionID", connectionInfo.ID(),
-		)
-
-		return &unixConnWrapper{
-			UnixConn:       conn,
-			connectionInfo: connectionInfo,
-		}, nil
-	default:
-		slog.Warn("listenerWrapper.Accept got unknown conn type",
-			"conn", conn,
-		)
-		return conn, nil
-	}
-}
-
-func createListener(
-	config config.ServerListenerConfiguration,
-) (net.Listener, error) {
-	if config.Network == "unix" {
-		os.Remove(config.ListenAddress)
-	}
-
-	listener, err := net.Listen(config.Network, config.ListenAddress)
-	if err != nil {
-		return nil, fmt.Errorf("net.Listen error: %w", err)
-	}
-
-	listenerWrapper := &listenerWrapper{
-		Listener: listener,
-	}
-
-	return listenerWrapper, nil
-}
 
 func addConnectionInfoToContext(
 	ctx context.Context,
