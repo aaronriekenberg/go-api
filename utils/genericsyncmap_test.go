67d70ecee09c8969df420c826c72fa4fd19e5231
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-02-28 05:19:45 -0600

    Update test.

--- utils/genericsyncmap_test.go
+++ utils/genericsyncmap_test.go
@@ -12,16 +12,35 @@ func TestStoreAndLoad(t *testing.T) {
 
 	value, ok := gsm.Load(1)
 	if ok != true || value != "one" {
-		t.Errorf("gsm.Load(1) = (%v, %v) want (\"one\", true)", ok, value)
+		t.Errorf("gsm.Load(1) = (%q, %v) want (\"one\", true)", value, ok)
 	}
 
 	value, ok = gsm.Load(2)
 	if ok != true || value != "two" {
-		t.Errorf("gsm.Load(1) = (%v, %v) want (\"two\", true)", ok, value)
+		t.Errorf("gsm.Load(1) = (%q, %v) want (\"two\", true)", value, ok)
 	}
 
 	value, ok = gsm.Load(3)
 	if ok != false || value != "" {
-		t.Errorf("gsm.Load(1) = (%v, %v) want (\"\", false)", ok, value)
+		t.Errorf("gsm.Load(1) = (%q, %v) want (\"\", true)", value, ok)
 	}
+}
+
+func TestLoadAndDelete(t *testing.T) {
+	var gsm GenericSyncMap[int, string]
+
+	gsm.Store(1, "one")
+	gsm.Store(2, "two")
+
+	value, ok := gsm.LoadAndDelete(1)
+	if ok != true || value != "one" {
+		t.Errorf("gsm.LoadAndDelete(1) = (%q, %v) want (\"\", true)", value, ok)
+
+	}
+
+	value, ok = gsm.LoadAndDelete(1)
+	if ok != false || value != "" {
+		t.Errorf("gsm.LoadAndDelete(1) = (%q, %v) want (\"\", false)", value, ok)
+	}
+
 }
