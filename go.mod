00545856b17f6ca616ef9ab9dea651522150b515
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-06-09 06:10:48 -0500

    Improve test.

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/BurntSushi/toml v1.5.0
 	github.com/aaronriekenberg/gsm v0.1.29
 	github.com/felixge/httpsnoop v1.0.4
-	github.com/google/go-cmp v0.7.0
 	golang.org/x/sync v0.15.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
--- request/request_test.go
+++ request/request_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 
 	"github.com/aaronriekenberg/go-api/request"
-
-	"github.com/google/go-cmp/cmp"
 )
 
 func TestIsExternal(
@@ -19,23 +17,31 @@ func TestIsExternal(
 		"aaronr.digital":           {host: "aaronr.digital", wantValue: true},
 		"notaaronr.digital":        {host: "notaaronr.digital", wantValue: false},
 		"www.aaronr.digital":       {host: "www.aaronr.digital", wantValue: true},
+		"aaronr.digital.com":       {host: "aaronr.digital.com", wantValue: false},
+		"www.aaronr.digital.com":   {host: "www.aaronr.digital.com", wantValue: false},
 		"stuff.www.aaronr.digital": {host: "stuff.www.aaronr.digital", wantValue: true},
 		".aaronr.digital":          {host: ".aaronr.digital", wantValue: true},
 		"Aaronr.Digital":           {host: "Aaronr.Digital", wantValue: true},
+		"Aaronr.Digital.Com":       {host: "Aaronr.Digital.Com", wantValue: false},
+		"Www.Aaronr.Digital.Com":   {host: "Www.Aaronr.Digital.Com", wantValue: false},
 		"NotAaronr.Digital":        {host: "NotAaronr.Digital", wantValue: false},
 		"Www.Aaronr.Digital":       {host: "Www.Aaronr.Digital", wantValue: true},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
+			if name != tc.host {
+				t.Fatalf("test name: %q host: %q name != tc.host", name, tc.host)
+
+			}
+
 			r := http.Request{
 				Host: tc.host,
 			}
 			value := request.IsExternal(&r)
 
-			diff := cmp.Diff(tc.wantValue, value)
-			if diff != "" {
-				t.Fatal(diff)
+			if value != tc.wantValue {
+				t.Fatalf("test name: %q host: %q got value: %v want value %v", name, tc.host, value, tc.wantValue)
 			}
 		})
 	}
