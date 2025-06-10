183b1a630fecda160a85b91c721a3aa18228d7d9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-06-10 05:09:58 -0500

    Remove host from struct, add more tests.

--- request/request_test.go
+++ request/request_test.go
@@ -11,35 +11,33 @@ func TestIsExternal(
 	t *testing.T,
 ) {
 	tests := map[string]struct {
-		host      string
 		wantValue bool
 	}{
-		"aaronr.digital":           {host: "aaronr.digital", wantValue: true},
-		"notaaronr.digital":        {host: "notaaronr.digital", wantValue: false},
-		"www.aaronr.digital":       {host: "www.aaronr.digital", wantValue: true},
-		"aaronr.digital.com":       {host: "aaronr.digital.com", wantValue: false},
-		"www.aaronr.digital.com":   {host: "www.aaronr.digital.com", wantValue: false},
-		"stuff.www.aaronr.digital": {host: "stuff.www.aaronr.digital", wantValue: true},
-		".aaronr.digital":          {host: ".aaronr.digital", wantValue: true},
-		"Aaronr.Digital":           {host: "Aaronr.Digital", wantValue: true},
-		"Notaaronr.Digital":        {host: "Notaaronr.Digital", wantValue: false},
-		"WWW.AARONR.DIGITAL":       {host: "WWW.AARONR.DIGITAL", wantValue: true},
-		"Aaronr.Digital.Com":       {host: "Aaronr.Digital.Com", wantValue: false},
+		"aaronr.digital":           {wantValue: true},
+		"notaaronr.digital":        {wantValue: false},
+		"www.aaronr.digital":       {wantValue: true},
+		"aaronr.digital.com":       {wantValue: false},
+		"www.aaronr.digital.com":   {wantValue: false},
+		"stuff.www.aaronr.digital": {wantValue: true},
+		".aaronr.digital":          {wantValue: true},
+		"Aaronr.Digital":           {wantValue: true},
+		"Notaaronr.Digital":        {wantValue: false},
+		"WWW.AARONR.DIGITAL":       {wantValue: true},
+		"Aaronr.Digital.Com":       {wantValue: false},
+		"Www.Aaronr.Dgital.Com":    {wantValue: false},
+		"Stuff.Www.Aaronr.Digital": {wantValue: true},
+		".AARONR.DIGITAL":          {wantValue: true},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-			if name != tc.host {
-				t.Fatalf("test name: %q host: %q name != tc.host", name, tc.host)
-			}
-
 			r := http.Request{
-				Host: tc.host,
+				Host: name,
 			}
 			value := request.IsExternal(&r)
 
 			if value != tc.wantValue {
-				t.Fatalf("test name: %q host: %q got value: %v want value %v", name, tc.host, value, tc.wantValue)
+				t.Fatalf("test: %q got value: %v want value %v", name, value, tc.wantValue)
 			}
 		})
 	}
